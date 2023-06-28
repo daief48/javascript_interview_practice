e3599d272a023731c5967b5f6fe1aaaa585947a0
daief48 <daief20103048@gmail.com>
2023-06-29 02:45:26 +0600

    Section 1 � values and variables

---
+++ main.js
@@ -0,0 +1,32 @@
+// Why JavaScript??
+// => JavaScript improves the user experience of the web pages by converting it from a static page into an interactive one. 
+
+// Section 1 👉 values and variables
+
+// var myName = "Daief Sikder"; // here var -> Variable (key), myName -> Name and "Daief Sikder" => value
+// console.log(myName)
+
+// Naming Variables: Rules And Best Practices 
+// 1. The first character must be a letter  or an underscore (_) or an doller($). You can't use a number as the first character
+// 2. The rest of the variable name can include any letter, any number,doller($), or the underscore. Can't use any other characters, including spaces.
+// 3. Variable names are case sensitive.
+// 4. No limit to the length of the variable name 
+// 5. You can't use one of the JavaScript's reserved words as a variable name. 
+
+// 1: Challenge Time: chosse the valid variable name  
+// var _myName = "Daief";
+// console.log(_myName);
+// var _1my__Name = "Daief"
+// console.log(_1my__Name);
+// var 1myName = "Daief"
+// console.log(1myName)
+// var $myName = "Daief Sikder"
+// console.log($myName);
+// var my$name = "Daief"
+// console.log(my$name);
+// var my$-name = "Daief"
+// console.log(my$-name);
+// var myname% = "Daief"
+// console.log(my$name%);
+
+// Section 2 👉 DATA TYPYES IN JAVASCRIPT

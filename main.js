31ce2a8cc46e3d2e49a4a7bdc381678b4c439cd4
daief48 <daief20103048@gmail.com>
2023-06-29 03:12:39 +0600

    Section 2 � DATA TYPYES IN JAVASCRIPT

--- main.js
+++ main.js
@@ -30,3 +30,67 @@
 // console.log(my$name%);
 
 // Section 2 👉 DATA TYPYES IN JAVASCRIPT
+// Six Data Types that are primitives 
+// 1. undefined: typeof instance === "undefined"
+// 2. Boolean: typeof instance === "boolean"
+// 3. Number: typeof instance === "number"
+// 4. String: typeof instance === "string"
+// 5. BigInt: typeof instance === "bigint"
+// 6. Symbol: typeof instance === "symbol"
+
+//  Examples:  
+// var myName = "Daief Sikder";
+// console.log(typeof(myName));
+// var myAge = 26;
+// console.log(typeof(myAge));
+// var isDaief = true;
+// console.log(typeof(isDaief));
+// const bigNum1 = 1234567890n;
+// console.log(typeof(bigNum1));
+// const mySymbol = Symbol('My Symbol');
+// console.log(typeof(mySymbol));
+
+// Challenge Time
+// console.log(10 + "20");
+// console.log(9 - "5"); // bug
+// console.log("Java" + "Script");
+// console.log(" " + " ");
+// console.log(" " + 0);
+// console.log("Daief" - "Sikder");
+// console.log(true + true);
+// console.log(true + false);
+// console.log(false + true);
+// console.log(false - true)
+// console.log(true - " ")
+
+// 🙋🧑‍🏫 Interview Question 1 🙋🧑‍🏫
+// Difference between null vs undefined
+// var iAmUseless = null;
+// console.log(iAmUseless);
+// console.log(typeof(iAmUseless));
+
+// var iAmStandBy;
+// console.log(iAmStandBy);
+// console.log(typeof(iAmStandBy));
+
+// 🙋🧑‍🏫 Interview Question 2 🙋🧑‍🏫
+// What is NaN?
+// NaN is a property of the global object
+// In other words, it is a variable in global scope.
+// The initial value of Nan is Not-A-Number.
+
+// var myPhoneNumber = 970707567;
+// var myName = "Daief Sikder";
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+
+// if(isNaN(myName)){
+//     console.log("Please enter your valid name.")
+// }
+
+// 3: Challenge Time 
+// console.log(NaN === NaN);
+// console.log(Number.NaN === NaN);
+// console.log(isNaN(NaN));
+// console.log(isNaN(Number.NaN));
+// console.log(Number.isNaN(NaN));
